79837c841dcb169c1040a46c49cdd3a0a1eff4fc
wxp123321 <1648557071@qq.com>
2018-07-13 17:30:52 +0800

    todos

---
+++ vue-webpack/vue-webpack/src/pages/Todos.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    this is todos
+    <router-link to="/"> home </router-link>
+    <router-link to="todos"> todos </router-link>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style scoped>
+
+</style>
--- vue-webpack/vue-webpack/src/router/index.js
+++ vue-webpack/vue-webpack/src/router/index.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
 
 Vue.use(Router)
 
@@ -8,8 +7,14 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'home',
+      component: () => import('@/pages/Home')
+    },
+
+    {
+      path: '/todos',
+      name: 'todos',
+      component: () => import('@/pages/Todos')
     }
   ]
 })
